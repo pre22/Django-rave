5b0c75d6adbede783ac22d68a137240776ff9936
Precious Chika <pre.chika.22@gmail.com>
2021-09-15 11:18:30 +0100

    chore: config redirect logic

--- flutterpay/payments/urls.py
+++ flutterpay/payments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from payments.views import HomePageView
+from payments.views import HomePageView, SuccessPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
+    path('success/', SuccessPageView.as_view(), name="payment_success"),
 ]
--- flutterpay/payments/views.py
+++ flutterpay/payments/views.py
@@ -1,12 +1,33 @@
 # from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.conf import settings
+import requests
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+
+
+
+
 
 class HomePageView(TemplateView):
     template_name = "payments/homepage.html"
 
     def get_context_data(self, **kwargs):
+        SEC_KEY = settings.RAVE_PRIVATE_KEY
+        headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {{ SEC_KEY }}'}
+        url = 'https://api.flutterwave.com/v3/transactions/t/verify'
+        resp = requests.get(url, headers = headers)
+
+
         context = super().get_context_data(**kwargs)
         context["key"] =  settings.RAVE_PUBLIC_KEY
+        if resp.status_code == "successful":
+            context["redirect_page"] = redirect(reverse("payment_success"))
+        else:
+            pass
         return context
     
+class SuccessPageView(TemplateView):
+    template_name = "payments/success.html"
