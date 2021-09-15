9482111b64940c502b6976abd4c6aea9db02abcc
Precious Chika <pre.chika.22@gmail.com>
2021-09-15 06:46:40 +0100

    chore: added key context data

--- flutterpay/payments/views.py
+++ flutterpay/payments/views.py
@@ -1,5 +1,12 @@
 # from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.conf import settings
 
 class HomePageView(TemplateView):
     template_name = "payments/homepage.html"
+
+    def def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["key"] =  settings.RAVE_PUBLIC_KEY
+        return context
+    
