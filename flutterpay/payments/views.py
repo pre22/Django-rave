3c539a81e29e96550c0d49e97fefe1f291a65421
Precious Chika <pre.chika.22@gmail.com>
2021-09-15 06:58:55 +0100

    chore: created success page

--- flutterpay/payments/views.py
+++ flutterpay/payments/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 class HomePageView(TemplateView):
     template_name = "payments/homepage.html"
 
-    def def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["key"] =  settings.RAVE_PUBLIC_KEY
         return context
